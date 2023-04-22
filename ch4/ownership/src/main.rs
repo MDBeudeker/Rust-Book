aeb82cf7cbee55374c37738fd78bb8d1c0d50e48
Maurits Beudeker <mauritsbeu@gmail.com>
2023-04-22 15:49:49 +0200

    Finished chapter 4.1-ownership

---
+++ ch4/ownership/src/main.rs
@@ -0,0 +1,33 @@
+/*
+//mutable strings
+fn main() {
+    let mut s = String::from("hello"); // this creates a mutable string
+
+    s.push_str(", world!");
+
+    println!("{}", s);
+}
+*/
+
+/*
+// string cloning
+fn main(){
+    let s1 = String:: from("hello!");
+
+    //let s2 = s1; // this kills the s1
+    let s2 = s1.clone();
+
+    println!("{s1}");
+}
+*/
+
+/*
+// this does not happen with integers though
+fn main(){
+    let x= 5;
+    let y= x; // the x is not killed here
+}
+
+*/
+
+
