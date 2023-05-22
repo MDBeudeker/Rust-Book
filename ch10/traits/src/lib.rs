e7560b7497f5714e8b6c0769f9bad31ed46ccab3
Maurits Beudeker <mauritsbeu@gmail.com>
2023-05-22 08:15:54 +0200

    ch10.2 finished

---
+++ ch10/traits/src/lib.rs
@@ -0,0 +1,41 @@
+pub trait Summary{
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle{
+    fn summarize (&self) -> String{
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet{
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet{
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+pub fn notify(item: &impl Summary){
+    println!("Breaking news! {}", item.summarize());
+}
+
+// fn some_function<T, U>(t: &T, u: &U) -> i32
+// where
+//     T: Display + Clone,
+//     U: Clone + Debug,
+// {
+
+// }
