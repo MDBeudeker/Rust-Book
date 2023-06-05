7a56d231b2f40aec8998bd497f1a92aa0c8d21a5
Maurits Beudeker <mauritsbeu@gmail.com>
2023-06-05 08:39:43 +0200

    finished chapter 11

--- ch11/adder/src/lib.rs
+++ ch11/adder/src/lib.rs
@@ -161,7 +161,7 @@ mod tests {
     }
 }
 */
-
+/* 
 // using Result<T,E>
 #[cfg(test)]
 mod tests {
@@ -175,4 +175,64 @@ mod tests {
     }
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+ */
 
+/* 
+// 11-02 multiple tests
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+}
+ */
+/* 
+#[test]
+fn it_works() {
+    assert_eq!(2 + 2, 4);
+}
+
+#[test]
+#[ignore]
+fn expensive_test() {
+    // code that takes an hour to run
+}
+*/
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+} 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
+    }
+}
